a02b45ccfb07cfa7c9789272256a74d8d21cfaca
artic0909 <alfag327@gmail.com>
2026-01-05 15:55:59 +0530

    sss

--- resources/views/manager/layouts/app.blade.php
+++ resources/views/manager/layouts/app.blade.php
@@ -97,20 +97,20 @@
 
     <!-- Alerts -->
     @if (session('success'))
-    <div id="successPopup" class="custom-success-popup">
-        {{ session('success') }}
-    </div>
+        <div id="successPopup" class="custom-success-popup">
+            {{ session('success') }}
+        </div>
     @endif
 
     @if (session('error'))
-    <div id="errorPopup" class="custom-error-popup">
-        {{ session('error') }}
-    </div>
+        <div id="errorPopup" class="custom-error-popup">
+            {{ session('error') }}
+        </div>
     @endif
 
 
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             const successPopup = document.getElementById('successPopup');
             const errorPopup = document.getElementById('errorPopup');
 
@@ -136,7 +136,7 @@
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+    <script src="{{ asset('assets/js/soft-ui-dashboard.min.js?v=1.0.3') }}"></script>
 </body>
 
 </html>
--- resources/views/manager/order-create.blade.php
+++ resources/views/manager/order-create.blade.php
@@ -113,7 +113,7 @@
             <div class="card mb-4">
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
+                        <table class="table align-items-center mb-0 text-nowrap">
                             <thead>
                                 <tr>
                                     <th
